97f91a14775576a1db0c11a82f78623ebfc31afd
AaronWinziers <awinziers@gmail.com>
2020-01-18 01:37:30 +0100

    Added some answers to old Algo questions

--- betriebssysteme/ue2java/src/Main.java
+++ betriebssysteme/ue2java/src/Main.java
@@ -1,66 +1,21 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+public class Dice {
 
-public class Main {
+	private final int seiten = 6;
+	private int ergebnis = 0;
 
-	public static void cputest(int number){
 
-		if (number < 2) return;
-
-		long start = System.currentTimeMillis();
-
-		boolean[] primes = new boolean[number];
-		for (int i = 0; i < number; i++) {
-			primes[i] = true;
-		}
-
-		for (int i = 2; i < number; i++) {
-			for (int j = 2; j < i; j++) {
-				if (i % j == 0){
-					primes[i] = false;
-					break;
-				}
-			}
-		}
-
-		long end = System.currentTimeMillis();
-
-		System.out.println("Calculating the primes took "+(end-start)+" ms");
+	public void throwDice() {
+		ergebnis = 1 + (int) (Math.random() * 6);
 	}
 
-	public static void iotest(int number) throws IOException {
-		long start = System.currentTimeMillis();
-
-		String filename = "file.txt";
-
-		for (int i = 0; i < number; i++) {
-			File file = new File(filename);
-			if (file.createNewFile()){
-				FileWriter writer = new FileWriter(filename);
-				for (int j = 0; j < 50; j++) {
-					writer.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget.");
-				}
-				if (file.delete()){
-					
-				}
-			}
+	public int pips() {
+		if (ergebnis == 0) {
+			throwDice();
 		}
-
-		long end = System.currentTimeMillis();
-
-		System.out.println("Creating, writing in and deleting the files took "+(end-start)+" ms");
+		return ergebnis;
 	}
 
-	public static void main(String[] args) {
-
-		cputest(250000);
-
-		try {
-			iotest(400000);
-		} catch (IOException e){
-			e.printStackTrace();
-		}
-
+	public static int pipsum(Dice d1, Dice d2) {
+		return d1.pips() + d2.pips();
 	}
 }
