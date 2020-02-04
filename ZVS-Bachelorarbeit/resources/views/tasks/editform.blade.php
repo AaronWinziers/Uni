1dc615b7060f7d3783820a7a3c2957887fe0d4a2
AaronWinziers <awinziers@gmail.com>
2020-02-04 09:54:18 +0100

    Removed stuff

--- ZVS-Bachelorarbeit/resources/views/tasks/editform.blade.php
+++
@@ -1,8 +0,0 @@
-
-{{ Form::open(['method' => 'GET', 'route' => 'log.edit' ]) }}
-{{Form::hidden('eid',$log->employeeID)}}
-{{Form::hidden('pid',$log->projectID)}}
-{{Form::hidden('tid',$log->taskNum)}}
-{{Form::hidden('ts',$log->timeStart)}}
-<button type="submit" class="btn btn-warning col-sm-9">Bearbeiten</button>
-{{ Form::close() }}
