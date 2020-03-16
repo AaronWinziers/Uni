f8ee808e7ef340079878d8f1f4045d8b3c3ff23d
AaronWinziers <awinziers@gmail.com>
2020-03-16 09:59:07 +0100

    Betriebssysteme Ausarbeitung

---
+++ betriebssysteme/ausarbeitung/increasing.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+for i in {0..9}; do 
+   date +%T > temp.csv
+   echo $1 >> temp.csv
+   echo $2 >> temp.csv
+   for n in {12..21}; do
+      if [ $n -ge 20 ]; then
+         time -f "%e" cp $1$(( 2 ** ($n % 20) ))GB.bin $2         2>> temp.csv
+      elif [ $n -ge 10 ]; then
+         time -f "%e" cp $1$(( 2 ** ($n % 10) ))MB.bin $2         2>> temp.csv
+      else 
+         time -f "%e" cp $1$(( 2 ** ($n) ))KB.bin $2              2>> temp.csv
+      fi
+   done
+   rm $2*
+   mv data.csv tempdata.csv
+   paste -d, tempdata.csv temp.csv > data.csv
+   rm temp.csv tempdata.csv
+done
---
+++ betriebssysteme/create.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+for n in {12..22}; do
+	if [ $n -ge 20 ]; then
+		dd if=/dev/urandom of=$(( 2 ** ($n % 20) ))GB.bin bs=1024 count=$(( 2 ** $n ))
+	elif [ $n -ge 10 ]; then
+                dd if=/dev/urandom of=$(( 2 ** ($n % 10) ))MB.bin bs=1024 count=$(( 2 ** $n ))
+	else 
+		dd if=/dev/urandom of=$(( 2 ** ($n) ))KB.bin bs=1024 count=$(( 2 ** $n ))
+	fi
+done
